8c6981c6cda0b24270fd8661dbc04b29cf19c6e3
AlmantasC <almantas.cincikas@mif.stud.vu.lt>
2026-02-18 13:09:27 +0200

    Added Median

--- main.cpp
+++ main.cpp
@@ -1,29 +1,37 @@
 #include <string>
 #include <iostream>
-#include <vector>
 #include <iomanip>
+#include <algorithm>
 using std::cin;
-using std::vector;
 using std::string;
 using std::cout;
 using std::setw;
 int main()
 {
-  struct studentas{
-      string vardas, pavarde;
-      vector <int> paz;
-      double rez=0;
-      };
-      int temp, n=0;
-      studentas A;
-      cin>>A.vardas>>A.pavarde;
-      while(cin>>temp) {
-        A.paz.push_back(temp);
-        A.rez+=A.paz[n];
-        n++;
-      };
-      A.rez/=n;
-      cout<<"Vardas"<<setw(10)<<"\tPavarde"<<setw(10)<<"\tGalutinis\n";
-      cout<<"--------------------------------------------\n";
-      cout<<A.vardas<<setw(10)<<'\t'<<A.pavarde<<setw(10)<<std::fixed<<std::setprecision(2)<<'\t'<<A.rez;
+    int m=2, n=3;
+    struct studentas{
+    string vardas, pavarde;
+    int* paz;
+    double rez=0;
+    double med;
+    };
+    studentas A[m];
+    for (int i=0; i<m; i++) {
+        A[i].paz = new int[n];
+        cin>>A[i].vardas>>A[i].pavarde;
+        for (int j=0; j<n; j++){
+            cin>>A[i].paz[j];
+            A[i].rez+=A[i].paz[j];
+        }
+        A[i].rez/=n;
+        std::sort(A[i].paz, A[i].paz+n);
+        if (n%2==0)
+        A[i].med=(A[i].paz[n/2-1]+A[i].paz[n/2])/2.0;
+        else
+        A[i].med=A[i].paz[n/2];
+    }
+    cout<<std::fixed<<std::setprecision(2)<<"Vardas\t\tPavarde\t\tGalutinis (Vid.) / Galutinis (Med.)\n";
+    for (int i=0; i<m; i++) {
+        cout<<A[i].vardas<<setw(10-A[i].vardas.length())<<'\t'<<A[i].pavarde<<setw(10-A[i].vardas.length())<<'\t'<<A[i].rez<<setw(10)<<'\t'<<A[i].med<<'\n';
+    }
 }
