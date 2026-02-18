5bf9dfe2101f421f7e7a79b2e8fa27db1b3cdc2d
AlmantasC <almantas.cincikas@mif.stud.vu.lt>
2026-02-18 11:27:49 +0200

    Paste pratybu darba

--- main.cpp
+++ main.cpp
@@ -1,8 +1,29 @@
+#include <string>
 #include <iostream>
-
+#include <vector>
+#include <iomanip>
+using std::cin;
+using std::vector;
+using std::string;
+using std::cout;
+using std::setw;
 int main()
 {
-  cout<<"Hello world";
-
-  return 0;
+  struct studentas{
+      string vardas, pavarde;
+      vector <int> paz;
+      double rez=0;
+      };
+      int temp, n=0;
+      studentas A;
+      cin>>A.vardas>>A.pavarde;
+      while(cin>>temp) {
+        A.paz.push_back(temp);
+        A.rez+=A.paz[n];
+        n++;
+      };
+      A.rez/=n;
+      cout<<"Vardas"<<setw(10)<<"\tPavarde"<<setw(10)<<"\tGalutinis\n";
+      cout<<"--------------------------------------------\n";
+      cout<<A.vardas<<setw(10)<<'\t'<<A.pavarde<<setw(10)<<std::fixed<<std::setprecision(2)<<'\t'<<A.rez;
 }
