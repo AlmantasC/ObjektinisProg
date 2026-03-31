b054d2fbc118275632d7408e137d36e6c34cd1f9
AlmantasC <almantas.cincikas@mif.stud.vu.lt>
2026-03-31 23:55:34 +0300

    Added timers to list

--- list/main.cpp
+++ list/main.cpp
@@ -56,9 +56,8 @@ int main(){
         case 3: generuotiViska(A, m); break;
         case 4: skaitytiIsFailo(A, m, failas); break;
     }
-    auto tNuskaitymas = chr::high_resolution_clock::now();
-    cout<<"\nNuskaitymas:       "<<chr::duration_cast<chr::milliseconds>(tNuskaitymas-tStart).count()<<" ms";
-
+    auto tNuskaitymasEnd = chr::high_resolution_clock::now();
+    cout << "\nNuskaitymas:   " << chr::duration_cast<chr::milliseconds>(tNuskaitymasEnd-tStart).count() << " ms";
     // --------- SKAICIAVIMAI ---------
 
     switch (skaiciavimas){
@@ -68,13 +67,14 @@ int main(){
 
 
     // --------- RUSIAVIMAS ---------
-
+    auto tRusiavimasStart = chr::high_resolution_clock::now();
     switch (rusiavimas) {
         case 1: A.sort(pagalVard); break;
         case 2: A.sort(pagalPavard); break;
         case 3: A.sort(pagalGal); break;
     }
-
+    auto tRusiavimasEnd = chr::high_resolution_clock::now();
+    cout << "\nRūšiavimas:    " << chr::duration_cast<chr::milliseconds>(tRusiavimasEnd-tRusiavimasStart).count() << " ms";
     // --------- ISVEDIMAS ---------
 
     switch (isvedimas) {
@@ -95,15 +95,12 @@ int main(){
             auto tSkirstymasEnd = chr::high_resolution_clock::now();
             cout<<"\nSkirstymas:        "<<chr::duration_cast<chr::milliseconds>(tSkirstymasEnd-tSkirstymasStart).count()<<" ms";
 
-            auto tIsvedasStart = chr::high_resolution_clock::now();
             std::ofstream fout("nevykeliai.txt");
             printRez(fout, nevykeliai, skaiciavimas);
             fout.close();
             std::ofstream foute("nerds.txt");
             printRez(foute, nerds, skaiciavimas);
             foute.close();
-            auto tIsvedasEnd = chr::high_resolution_clock::now();
-            cout<<"\nIsvedimas i faila: "<<chr::duration_cast<chr::milliseconds>(tIsvedasEnd-tIsvedasStart).count()<<" ms";
             }
         }
 
